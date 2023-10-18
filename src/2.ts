0f1d0433605870c5baaa372b2203f531253094f5
VorontsovaYulia <vorontsovayulia89@gmail.com>
2023-10-19 00:49:27 +0300

    fixed task 2

--- src/2.ts
+++ src/2.ts
@@ -21,15 +21,14 @@ class Employee {
   getEmployeeDetails() {
     return `Name: ${this.name}, Department: ${this.department}, Salary: ${this.salary}`;
   }
-}
+};
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
-  constructor() {
-    super("a", "c", 10)
-    this.salary += 10000;
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary + 10000);
   }
-}
+};
 
 
 export {};
